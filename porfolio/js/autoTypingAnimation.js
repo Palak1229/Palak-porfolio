07cfccc6ce0507688ec0cb9e8e8e8c897fcce878
palak verma <88045393+Palak1229@users.noreply.github.com>
2023-10-05 11:17:31 +0530

    Add files via upload

---
+++ porfolio/js/autoTypingAnimation.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: ["<span> Hi, I am Pushpraj Kushwaha </span> Working in Back End Development , Enthusiastic Technology with a demonstrated history of learning in the information technology and services industry. Skilled in Back End Tech, JAVA , Web development with multiple tools and technologies . B.Tech focused in Computer Science from Gyan ganga intitute of technology and science , Jabalpur MP "],
+      typeSpeed: 01,
+      contentType: 'html'
+      
+    });
+  }, 1000);
